6d53e32ce5f20a2322aaa75978eb9b7e1b2e7f6e
nimakarimipour <karimipour.nima@gmail.com>
2020-07-07 14:41:40 -0500

    added structure

--- src/test/java/BasicTest.java
+++ src/test/java/BasicTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import edu.riple.annotationinjector.Injector;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class BasicTest {
+
+    Injector injector;
+    String srcPath = "/Desktop/Super.java";
+    String fixPath = "/Desktop/fixes.java";
+
+    @Before
+    public void setup(){
+
+        injector = Injector.builder()
+                .addPath(srcPath)
+                .setFixesJsonFilePath(fixPath)
+                .build();
+    }
+
+    @Test
+    public void basicTest(){
+        System.out.println(injector.start());
+    }
 }
