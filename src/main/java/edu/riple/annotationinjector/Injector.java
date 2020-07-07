a5f872df62974b8255bbce7076d3f5d6a0ca3f8e
nimakarimipour <karimipour.nima@gmail.com>
2020-07-07 14:39:32 -0500

    added structure

---
+++ src/main/java/edu/riple/annotationinjector/Injector.java
@@ -0,0 +1,55 @@
+package edu.riple.annotationinjector;
+
+import org.openrewrite.java.Java8Parser;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.tree.J;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
+public class Injector{
+
+    JavaParser parser;
+    ArrayList<Path> paths;
+    Path fixesFilePath;
+
+    Injector() {
+        parser = Java8Parser.builder().build();
+        paths = new ArrayList<>();
+    }
+
+    public static InjectorBuilder builder(){
+        return new InjectorBuilder();
+    }
+
+    public boolean start() {
+        ArrayList<J.CompilationUnit> trees = (ArrayList<J.CompilationUnit>) parser.parse(paths);
+        for (J.CompilationUnit unit : trees){
+            System.out.println(unit.getImports());
+        }
+        return true;
+    }
+
+    public static class InjectorBuilder{
+        private final Injector injector;
+
+        public InjectorBuilder(){
+            injector = new Injector();
+        }
+
+        public InjectorBuilder addPath(String path){
+            injector.paths.add(Paths.get(path));
+            return this;
+        }
+
+        public InjectorBuilder setFixesJsonFilePath(String path){
+            injector.fixesFilePath = Paths.get(path);
+            return this;
+        }
+
+        public Injector build(){
+            return injector;
+        }
+    }
+}
