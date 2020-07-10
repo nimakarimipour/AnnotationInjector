056feed1aea6645767d9e0e5dea699dc39584c74
nimakarimipour <karimipour.nima@gmail.com>
2020-07-09 22:00:40 -0500

    added tools

--- src/test/java/edu/riple/annotationinjector/BasicTest.java
+++ src/test/java/edu/riple/annotationinjector/BasicTest.java
@@ -1,5 +1,6 @@
 package edu.riple.annotationinjector;
 
+import edu.riple.annotationinjector.tools.InjectorTestHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,15 +11,18 @@ import org.junit.runners.JUnit4;
 public class BasicTest {
 
     Injector injector;
+    InjectorTestHelper testHelper;
     String fixPath = "/Users/nima/Developer/AnnotationInjector/testfiles/fixes.json";
 
     @Before
     public void setup(){
+        testHelper = new InjectorTestHelper();
         injector = Injector.builder().setFixesJsonFilePath(fixPath).build();
     }
 
     @Test
     public void basicTest(){
+
         System.out.println("Injector final output: " + injector.start());
     }
 }
---
+++ src/test/java/edu/riple/annotationinjector/tools/InjectorTestHelper.java
@@ -0,0 +1,5 @@
+package edu.riple.annotationinjector.tools;
+
+public class InjectorTestHelper {
+
+}
