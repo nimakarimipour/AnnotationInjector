95bf7533a6915bd78ba91fad07ea4c4d4cf86c96
nimakarimipour <karimipour.nima@gmail.com>
2020-07-30 11:53:35 -0500

    added main file for quick runs

---
+++ src/main/java/edu/riple/annotationinjector/Main.java
@@ -0,0 +1,27 @@
+package edu.riple.annotationinjector;
+
+import org.openrewrite.java.Java8Parser;
+import org.openrewrite.java.JavaParser;
+import org.openrewrite.java.tree.J;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Main {
+  public static void main(String[] args) {
+    JavaParser parser =
+            Java8Parser.builder()
+                    .relaxedClassTypeMatching(true)
+                    .logCompilationWarningsAndErrors(false)
+                    .build();
+
+
+    String uri = "/Users/nima/Developer/WALA/com.ibm.wala.core/src/main/java/com/ibm/wala/analysis/typeInference/TypeInference.java";
+    ArrayList<Path> p = new ArrayList<>();
+    p.add(Paths.get(uri));
+    ArrayList<J.CompilationUnit> trees = (ArrayList<J.CompilationUnit>) parser.parse(p);
+    J.CompilationUnit tree = trees.get(0);
+    System.out.println(tree.print());
+  }
+}
