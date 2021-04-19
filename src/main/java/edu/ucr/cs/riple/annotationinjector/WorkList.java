fe77daa5e8a8df9a815cb6a116f97a0dbd7fb88a
nimakarimipour <karimipour.nima@gmail.com>
2021-04-18 18:27:34 -0700

    update

--- src/main/java/edu/ucr/cs/riple/annotationinjector/WorkList.java
+++ src/main/java/edu/ucr/cs/riple/annotationinjector/WorkList.java
@@ -1,14 +1,21 @@
 package edu.ucr.cs.riple.annotationinjector;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class WorkList {
   private final String uri;
-  private final ArrayList<Fix> fixes;
+  private final List<Fix> fixes;
 
   public WorkList(String uri) {
     this.uri = uri;
     this.fixes = new ArrayList<>();
+  }
+
+  public WorkList(List<Fix> fixes) {
+    this.fixes = fixes;
+    assert fixes.size() > 0;
+    uri = fixes.get(0).uri;
   }
 
   public void addFix(Fix newFix) {
@@ -16,12 +23,16 @@ public class WorkList {
     fixes.add(newFix);
   }
 
-  public ArrayList<Fix> getFixes() {
+  public List<Fix> getFixes() {
     return fixes;
   }
 
   public String getUri() {
     return uri;
+  }
+
+  public void addContainingAnnotationsToList(List<String> annotsList) {
+    for (Fix fix : fixes) if (!annotsList.contains(fix.annotation)) annotsList.add(fix.annotation);
   }
 
   public String className(){
